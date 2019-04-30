9305bc896641e790fe7751a9adc74a9d68d7502b
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-30 12:09:29 +0200

    Boletin31 C

---
+++ src/boletin31c/IntegranteInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin31c;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public interface IntegranteInterface {
+    
+    public void concentrarse();
+    
+    public void viajar();
+    
+}
---
+++ src/boletin31c/MasajistaHM.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin31c;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class MasajistaHM extends SeleccionFutbolHM implements IntegranteInterface{
+    String titulacion;
+    int aniosExperiencia;
+
+    public MasajistaHM() {
+        
+    }
+
+    public MasajistaHM(String titulacion, int aniosExperiencia, int id, int edad, String nombre, String apellidos) {
+        super(id, edad, nombre, apellidos);
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+    
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String getApellidos() {
+        return apellidos;
+    }
+    
+    public void darMasaje(){}
+
+    @Override
+    public void concentrarse(){
+    System.out.println("O masajista "+ nombre+" "+apellidos + " concentrase");
+    }
+
+    @Override
+    public void viajar(){}
+
+
+    @Override
+    public String toString() {
+        return super.toString() + "Titulacion= " + titulacion + "\nAniosExperiencia= " + aniosExperiencia;
+    }
+    
+    
+}
